f0c3d39f80e7ab632c9c642ca49b1349ccaba762
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-03 00:57:58 +0300

    devsync test commit

--- src/pages/GitHubCallbackPage.tsx
+++ src/pages/GitHubCallbackPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Box, CircularProgress, Typography, Alert, Button } from '@mui/material';
 import { authService } from '../services/authService';
