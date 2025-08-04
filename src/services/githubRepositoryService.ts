894e9924254c9f7e0ea51c854e8d7e0964884228
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-04 23:52:14 +0300

    test

--- src/services/githubRepositoryService.ts
+++ src/services/githubRepositoryService.ts
@@ -5,11 +5,9 @@ import { authService } from './authService';
 export const githubRepositoryService = {
     getUserRepositories: async (username: string): Promise<RepositoryFromApi[]> => {
         try {
-            // Token kontrolü
             if (!authService.isAuthenticated()) {
                 throw new Error('User is not authenticated');
             }
-
             const response = await apiClient.get<RepositoryFromApi[]>(`/git/${username}`);
             return response.data;
         } catch (error: any) {
