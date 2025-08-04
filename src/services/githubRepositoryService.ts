48f87b4cdbb81d18132574b4b13ad3bda7406d39
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-04 22:22:07 +0300

    some changes

--- src/services/githubRepositoryService.ts
+++ src/services/githubRepositoryService.ts
@@ -13,14 +13,6 @@ export const githubRepositoryService = {
             const response = await apiClient.get<RepositoryFromApi[]>(`/git/${username}`);
             return response.data;
         } catch (error: any) {
-            // Eğer 403 hatası alırsak, kullanıcıyı login sayfasına yönlendirelim
-            if (error.response?.status === 403) {
-                authService.logout(); // Token'ları temizle
-                window.location.href = '/login'; // Login sayfasına yönlendir
-                throw new Error('Session expired. Please login again.');
-            }
-            
-            console.error('Error fetching user repositories:', error);
             throw error;
         }
     }
